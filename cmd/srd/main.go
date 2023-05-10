07996d9122671161649861eaf0ac9ebdb821f07e
Yifei Sun <ysun@hey.com>
2023-05-10 17:04:38 -0600

    add demo

--- cmd/srd/main.go
+++ cmd/srd/main.go
@@ -5,30 +5,84 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"time"
+	"os"
 
 	"github.com/charmbracelet/log"
 )
 
-func main() {
-	for i := 1; i <= 100; i++ {
-		log.Info(fmt.Sprintf("Running %d/100...", i))
-		time.Sleep(10 * time.Millisecond)
-	}
+type State struct {
+	Goroutines map[string]bool
+	Memory     map[string]int
+	Channels   map[string][]bool
+}
 
-	//Create a FileSet to work with
+type Visitor struct {
+	State *State
+}
+
+func (v *Visitor) Visit(node ast.Node) ast.Visitor {
+	switch n := node.(type) {
+	case *ast.GoStmt:
+		// Goroutine Creation Rule
+		v.State.Goroutines[fmt.Sprint(n.Call.Fun)] = true
+	case *ast.IncDecStmt:
+		// Memory Modification Rule
+		// (Assuming this occurs inside a goroutine for simplicity)
+		for goroutine := range v.State.Goroutines {
+			v.State.Memory[goroutine]++
+		}
+	case *ast.SendStmt:
+		// Channel Send Rule
+		v.State.Channels[n.Chan.(*ast.Ident).Name] = append(v.State.Channels[n.Chan.(*ast.Ident).Name], true)
+	case *ast.UnaryExpr:
+		if n.Op == token.ARROW {
+			// Channel Receive Rule
+			v.State.Channels[n.X.(*ast.Ident).Name] = v.State.Channels[n.X.(*ast.Ident).Name][1:]
+		}
+	case *ast.CallExpr:
+		// Print Rule (Assuming all calls are print statements for simplicity)
+		log.Info(n.Fun)
+	}
+	return v
+}
+
+func main() {
+	filename := os.Args[len(os.Args)-1]
+
+	// Create a FileSet to work with
 	fset := token.NewFileSet()
-	//Parse the file and create an AST
-	file, err := parser.ParseFile(fset, "./cmd/srd/main.go", nil, parser.ParseComments)
+
+	// Parse the file and create an AST
+	file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
 	if err != nil {
 		panic(err)
 	}
-	ast.Inspect(file, func(n ast.Node) bool {
-		// Find Function Call Statements
-		funcCall, ok := n.(*ast.CallExpr)
-		if ok {
-			fmt.Println(funcCall.Fun)
+
+	// Initialize the state and visitor
+	state := &State{
+		Goroutines: make(map[string]bool),
+		Memory:     make(map[string]int),
+		Channels:   make(map[string][]bool),
+	}
+	visitor := &Visitor{State: state}
+
+	// Use the visitor to traverse the AST
+	ast.Walk(visitor, file)
+
+	// Print out the state after traversal
+	log.Info("Goroutines:", visitor.State.Goroutines)
+	log.Info("Memory:", visitor.State.Memory)
+	log.Info("Channels:", visitor.State.Channels)
+
+	// Check for potential data races
+	dataRace := false
+	for location, accesses := range visitor.State.Memory {
+		if accesses > 1 {
+			log.Info("Potential data race at memory location:", location)
+			dataRace = true
 		}
-		return true
-	})
+	}
+	if !dataRace {
+		log.Info("No potential data races detected.")
+	}
 }
